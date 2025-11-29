3ff050182057a643564613edd1f50b7f7c00be03
Roman <osmusshin00@gmail.com>
2025-11-29 20:21:58 +0500

    fixes masks

--- src/masks.py
+++ src/masks.py
@@ -11,7 +11,7 @@ def get_mask_card_number(card: str) -> str:
     masked = left_visible + "******" + right_visible
 
     # Разбиваем на блоки по 4 символа
-    groups = [masked[i : i + 4] for i in range(0, len(masked), 4)]
+    groups = [masked[i: i + 4] for i in range(0, len(masked), 4)]
 
     return " ".join(groups)
 
